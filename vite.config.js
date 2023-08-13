590ab2580ca9652cd87fb413bded86ecca712f83
acrespo <arturoelias159@gmail.com>
2023-08-13 19:03:37 -0400

    Configruacion GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://https://arturocrespocassier.github.io/desafio-CCU/'
+  base: '/desafio-CCU/'
 })
