4b4e8de406c14f315a7ca9af279ebe8efc93455c
acrespo <arturoelias159@gmail.com>
2023-08-13 19:44:09 -0400

    Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
-import SearchMovies from './components/searchMovies'
+import {SearchMovies}  from './components/searchMovies'
 import { Modal } from './components/Modal';
 
 
